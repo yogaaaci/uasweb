17c5aaf5591418405a22fe89f36e693b62314051
LioNate00 <131903803+LioNate00@users.noreply.github.com>
2023-07-16 14:45:42 +0700

    Create phplogin.php

---
+++ Login/phplogin.php
@@ -0,0 +1,19 @@
+<?php
+$server="LocalHost";
+$username="root";
+$password="";
+$db = "university_db";
+
+$connect=mysql_connect($server,$username,$password);
+$select_db=mysqli_select_db($db);
+if($connect){
+    echo "Berhasil terkoneksi";
+        if($select_db){
+            echo "Database ditemukan";
+        } else {
+            echo "Database tidak ditemukan";
+        }
+} else {
+    echo "Gagal terkoneksi";
+}
+?>
